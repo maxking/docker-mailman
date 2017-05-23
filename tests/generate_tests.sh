5954d03c349012e20155cf09fc628d92ff22fa7f
Abhilash Raj <raj.abhilash1@gmail.com>
2017-05-23 15:59:20 -0700

    Test should test the current branch version and not the 'latest'.

---
+++ tests/generate_tests.sh
@@ -0,0 +1,15 @@
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+
+
+cat > docker-test.yaml <<EOF
+services:
+  mailman-core:
+    image:maxking/mailman-core:$TAG
+
+  mailman-web:
+    image:maxking/mailman-web:$TAG
+EOF
