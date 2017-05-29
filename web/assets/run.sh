8efd224cf91ef44780849b1d9fbae673e1ec4c2a
Abhilash Raj <raj.abhilash1@gmail.com>
2017-05-28 17:59:40 -0700

    Add user in the image and not container.

--- web/assets/run.sh
+++ web/assets/run.sh
@@ -91,7 +91,6 @@ python manage.py migrate
 
 # Create a mailman user with the specific UID and GID and do not create home
 # directory for it. Also chown the logs directory to write the files.
-useradd -M -U -u 1000 mailman
 chown mailman:mailman /opt/mailman-web-data -R
 
 
