a361b31923034afd2eacea92604ea673a358e7f2
Abhilash Raj <raj.abhilash1@gmail.com>
2017-07-18 17:47:58 -0700

    Switch ports for uwsgi and http.

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,7 @@ set -e
 curl -u restadmin:restpass http://172.19.199.2:8001/3.1/system
 
 # Check to see if postorius is working.
-curl -L http://172.19.199.3:8080/postorius/lists | grep "Mailing List"
+curl -L http://172.19.199.3:8000/postorius/lists | grep "Mailing List"
 
 # Check to see if hyperkitty is working.
-curl -L http://172.19.199.3:8080/hyperkitty/ | grep "Available lists"
+curl -L http://172.19.199.3:8000/hyperkitty/ | grep "Available lists"
