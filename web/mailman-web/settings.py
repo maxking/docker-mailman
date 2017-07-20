43731112e7b69bac6038875864a68d31703b8348
Abhilash Raj <raj.abhilash1@gmail.com>
2017-07-20 15:03:20 -0700

    Remove Facebook from default social auth provider.

--- web/mailman-web/settings.py
+++ web/mailman-web/settings.py
@@ -92,8 +92,6 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.gitlab',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook',
-    #'allauth.socialaccount.providers.stackexchange',
 )
 
 
