93be3203a51995b2cbebd0243e19bfbd2229978a
Abhilash Raj <raj.abhilash1@gmail.com>
2017-08-06 10:03:28 -0700

    Use VERSION to build images in automated fashion

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push maxking/mailman-web
-docker push maxking/mailman-core
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]
+then
+	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+	docker push maxking/mailman-web
+	docker push maxking/mailman-core
+else
+	echo "Non-master branch aren't pushed to DockerHub..."
+fi
