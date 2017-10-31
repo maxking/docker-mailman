b8a0e82d5e658aa049994b8fdfbddb5f6b838d57
Patrick Gansterer <paroga@paroga.com>
2017-10-31 13:54:17 +0100

    Change hyperkitty and postorius paths to more general names

--- tests/test.sh
+++ tests/test.sh
@@ -29,7 +29,7 @@ docker logs mailman-core
 curl -u restadmin:restpass http://172.19.199.2:8001/3.1/system
 
 # Check to see if postorius is working.
-curl -L http://172.19.199.3:8000/postorius/lists | grep "Mailing List"
+curl -L http://172.19.199.3:8000/mailman3/lists | grep "Mailing List"
 
 # Check to see if hyperkitty is working.
-curl -L http://172.19.199.3:8000/hyperkitty/ | grep "Available lists"
+curl -L http://172.19.199.3:8000/archives/ | grep "Available lists"
