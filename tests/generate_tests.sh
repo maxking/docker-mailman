9b4b856b9341a1ae67b5df6876b34e66fda1fa75
Abhilash Raj <raj.abhilash1@gmail.com>
2017-11-02 01:07:24 -0700

    Add healthcheck to core.

--- tests/generate_tests.sh
+++ tests/generate_tests.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
 if [ "$TRAVIS" ]; then
-		if [ "$TRAVIS_BRANCH" = "master" ]; then
-				TAG="latest"
-		else
-				TAG="$TRAVIS_BRANCH"
-		fi
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        TAG="latest"
+    else
+        TAG="$TRAVIS_BRANCH"
+    fi
 elif [ "$CIRCLECI" ]; then
-		if [ "$CIRCLE_BRANCH" = "master" ]; then
-				TAG="latest"
-		else
-				TAG="$CIRCLE_BRANCH"
-		fi
+    if [ "$CIRCLE_BRANCH" = "master" ]; then
+        TAG="latest"
+    else
+        TAG="$CIRCLE_BRANCH"
+    fi
 fi
 
 
 
 cat > docker-test.yaml <<EOF
-version: '2'
+version: '2.1'
 
 services:
   mailman-core:
     image: maxking/mailman-core:$TAG
+    healthcheck:
+      test: ["CMD", "curl", "-f", "http://localhost:8001/3.1/system"]
+      interval: 30s
+      timeout: 5s
+      retries: 3
+      start_period: 30s
 
   mailman-web:
     image: maxking/mailman-web:$TAG
