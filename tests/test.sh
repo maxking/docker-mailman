bee6dd3872e790f6dc30bdc5ef214e058ed98444
Abhilash Raj <raj.abhilash1@gmail.com>
2017-11-02 01:59:22 -0700

    Fix tests.

--- tests/test.sh
+++ tests/test.sh
@@ -29,7 +29,7 @@ docker logs mailman-core
 docker exec -it mailman-core curl -u restadmin:restpass http://172.19.199.2:8001/3.1/system
 
 # Check to see if postorius is working.
-docker-exec -it mailman-web curl -L http://172.19.199.3:8000/postorius/lists | grep "Mailing List"
+docker exec -it mailman-web curl -L http://172.19.199.3:8000/postorius/lists | grep "Mailing List"
 
 # Check to see if hyperkitty is working.
-docker-exec -it mailman-web curl -L http://172.19.199.3:8000/hyperkitty/ | grep "Available lists"
+docker exec -it mailman-web curl -L http://172.19.199.3:8000/hyperkitty/ | grep "Available lists"
