38e7bba4a1dd23fed873479b069ce46f89d51a36
Abhilash Raj <raj.abhilash1@gmail.com>
2017-11-03 19:27:34 -0700

    Fix deploy scripts to select correct tags.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+function deploy {
+		docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+		docker push maxking/mailman-web:$1
+		docker push maxking/mailman-core:$1
+}
 
 
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ ! -z $DEV ] ; then
-		TAG="rolling"
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+		deploy "rolling"
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
-		TAG="latest"
+		deploy "latest"
 else
 		# If the branch isn't master and this was not a cron job, no need to deploy.
+		echo "TRAVIS_EVENT_TYPE = $TRAVIS_EVENT_TYPE, TRAVIS_BRANCH = $TRAVIS_BRANCH"
 		exit 0
 fi
-
-
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push maxking/mailman-web:$TAG
-docker push maxking/mailman-core$TAG
