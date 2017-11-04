2a171fd3dbc268634ace124e274c9b1ad32d6907
Abhilash Raj <raj.abhilash1@gmail.com>
2017-11-03 18:58:42 -0700

    Simplify the deployment logic.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 
-if [ "$TRAVIS_BRANCH" = "master" ]
-then
-	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-	docker push maxking/mailman-web
-	docker push maxking/mailman-core
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ ! -z $DEV ] ; then
+		TAG="rolling"
+elif [ "$TRAVIS_BRANCH" = "master" ]; then
+		TAG="latest"
 else
-	echo "Non-master branch aren't pushed to DockerHub..."
+		# If the branch isn't master and this was not a cron job, no need to deploy.
+		exit 0
 fi
+
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker push maxking/mailman-web:$TAG
+docker push maxking/mailman-core$TAG
