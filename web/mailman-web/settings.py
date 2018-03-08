248a52c7b57477957fcca2ad1a61919695da0015
Abhilash Raj <raj.abhilash1@gmail.com>
2018-03-07 22:16:05 -0800

    Use new style middleware

--- web/mailman-web/settings.py
+++ web/mailman-web/settings.py
@@ -100,7 +100,7 @@ INSTALLED_APPS = (
 )
 
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
