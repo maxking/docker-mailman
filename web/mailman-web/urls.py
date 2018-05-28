b777736b8efcb366fa37265029dbb9b0afbfb614
Abhilash Raj <maxking@users.noreply.github.com>
2018-05-28 15:21:52 -0700

    Use the new django.urls API

--- web/mailman-web/urls.py
+++ web/mailman-web/urls.py
@@ -19,7 +19,7 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
