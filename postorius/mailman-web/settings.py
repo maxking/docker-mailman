5dbeb4239ee7b06740e10f6a90591b950789d626
Danil Smirnov <danil@smirnov.la>
2020-01-08 18:28:45 +0000

    Getting rid of django_compressor

--- postorius/mailman-web/settings.py
+++ postorius/mailman-web/settings.py
@@ -194,7 +194,6 @@ STATIC_URL = '/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 )
 
 
