47f70d73bdfcd1355e00acca41afda118f98c245
Stefan Gangefors <stefan@gangefors.se>
2021-03-20 18:28:54 +0100

    Easy override for social logins (#446)
    Add MAILMAN_WEB_SOCIAL_AUTH for social logins
    
    The `MAILMAN_WEB_SOCIAL_AUTH` list contains a default set of social
    login provides. This was previously included in `INSTALLED_APPS`.
    
    Separating it to it's own list makes it easier to disable or otherwise
    override which social login providers are enabled without the need to
    modify `INSTALLED_APPS`.
    
    Older installations where `INSTALLED_APPS` were overridden continues to
    work. The `MAILMAN_WEB_SOCIAL_AUTH` is ignored for those and
    `INSTALLED_APPS` is used as is.

--- web/mailman-web/settings.py
+++ web/mailman-web/settings.py
@@ -66,7 +66,8 @@ MAILMAN_ARCHIVER_FROM = (os.environ.get('MAILMAN_HOST_IP', gethostbyname(os.envi
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = []
+DEFAULT_APPS = [
     'hyperkitty',
     'postorius',
     'django_mailman3',
@@ -89,22 +90,14 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+]
+MAILMAN_WEB_SOCIAL_AUTH = [
     'django_mailman3.lib.auth.fedora',
     'allauth.socialaccount.providers.openid',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.gitlab',
     'allauth.socialaccount.providers.google',
 ]
-
-# Optionally include paintstore, if it was installed with Hyperkitty.
-# TODO: Remove this after a new version of Hyperkitty is released and
-# neither the stable nor the rolling version needs it.
-try:
-    import paintstore
-    INSTALLED_APPS.append('paintstore')
-except ImportError:
-    pass
-
 
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -412,3 +405,7 @@ try:
     from settings_local import *
 except ImportError:
     pass
+
+# Compatibility for older installs that override INSTALLED_APPS
+if not INSTALLED_APPS:
+    INSTALLED_APPS = DEFAULT_APPS + MAILMAN_WEB_SOCIAL_AUTH
