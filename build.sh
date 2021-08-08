1236463effbbb78d8f05d9846cc4b58465efaeaa
Colin Dawson <me@cjdawson.com>
2021-08-08 21:31:12 +0100

    Changed the code over to use manifests instead of buildx

--- build.sh
+++ build.sh
@@ -24,18 +24,21 @@ if [ "$EVENT_TYPE" = "cron" ]  || [ "$DEV" = "true" ]; then
     DJ_MM3_REF=$(python get_latest_ref.py mailman/django-mailman3)
     MM3_HK_REF=$(python get_latest_ref.py mailman/mailman-hyperkitty)
 
-    # Build the mailman-core image.
-    $DOCKER buildx build -f core/Dockerfile.dev \
+	for value in amd64 arm32v7 arm64v8
+	do
+		# Build the mailman-core image.
+		$DOCKER build -f core/Dockerfile.dev \
+	        --build-arg ARCH=$value \
             --build-arg CORE_REF=$CORE_REF \
             --build-arg MM3_HK_REF=$MM3_HK_REF \
             --label version.core="$CORE_REF" \
             --label version.mm3-hk="$MM3_HK_REF" \
             --label version.git_commit="$COMMIT_ID" \
-            --platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	        --tag maxking/mailman-core:rolling core/
+	        -t maxking/mailman-core:rolling-$value core/
 
-    # Build the mailman-web image.
-    $DOCKER buildx build -f web/Dockerfile.dev \
+		# Build the mailman-web image.
+		$DOCKER build -f web/Dockerfile.dev \
+	        --build-arg ARCH=$value \
             --label version.git_commit="$COMMIT_ID" \
             --label version.postorius="$POSTORIUS_REF" \
             --label version.hyperkitty="$HYPERKITTY_REF" \
@@ -45,10 +48,10 @@ if [ "$EVENT_TYPE" = "cron" ]  || [ "$DEV" = "true" ]; then
             --build-arg CLIENT_REF=$CLIENT_REF \
             --build-arg HYPERKITTY_REF=$HYPERKITTY_REF \
             --build-arg DJ_MM3_REF=$DJ_MM3_REF \
-			--platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	        --tag maxking/mailman-web:rolling web/
+	        -t maxking/mailman-web:rolling-$value web/
 
-    $DOCKER buildx build -f postorius/Dockerfile.dev\
+		$DOCKER build -f postorius/Dockerfile.dev\
+	        --build-arg ARCH=$value \
 			--label version.git_commit="$COMMIT_ID"\
             --label version.postorius="$POSTORIUS_REF" \
             --label version.client="$CLIENT_REF" \
@@ -56,17 +59,38 @@ if [ "$EVENT_TYPE" = "cron" ]  || [ "$DEV" = "true" ]; then
             --build-arg POSTORIUS_REF=$POSTORIUS_REF \
             --build-arg CLIENT_REF=$CLIENT_REF \
             --build-arg DJ_MM3_REF=$DJ_MM3_REF \
-			--platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	        --tag maxking/postorius:rolling postorius/
+	        -t maxking/postorius:rolling-$value postorius/
+	done
 else
-    # Do the normal building process.
-    $DOCKER buildx build \
-	  --platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	  --tag maxking/mailman-core:rolling core/
-    $DOCKER buildx build \
-	  --platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	  --tag maxking/mailman-web:rolling web/
-    $DOCKER buildx build \
-	  --platform linux/arm/v7,linux/arm64/v8,linux/amd64
-	  --tag maxking/postorius:rolling postorius/
+	for value in amd64 arm32v7 arm64v8
+	do
+		# Do the normal building process.
+		$DOCKER build \
+		--build-arg ARCH=$value \
+		-t maxking/mailman-core:rolling-$value core/
+		$DOCKER build \
+		--build-arg ARCH=$value \
+		-t maxking/mailman-web:rolling-$value web/
+		$DOCKER build \
+		--build-arg ARCH=$value \
+		-t maxking/postorius:rolling-$value postorius/
+	done
 fi
+
+docker manifest create \
+	maxking/mailman-core:rolling \
+	--amend maxking/mailman-core:rolling-amd64 \
+	--amend maxking/mailman-core:rolling-arm32v7 \
+	--amend maxking/mailman-core:rolling-arm64v8
+
+docker manifest create \
+	maxking/mailman-web:rolling \
+	--amend maxking/mailman-web:rolling-amd64 \
+	--amend maxking/mailman-web:rolling-arm32v7 \
+	--amend maxking/mailman-web:rolling-arm64v8
+
+docker manifest create \
+	maxking/postorius:rolling \
+	--amend maxking/postorius:rolling-amd64 \
+	--amend maxking/postorius:rolling-arm32v7 \
+	--amend maxking/postorius:rolling-arm64v8
