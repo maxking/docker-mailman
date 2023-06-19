72b6bc8b61769bb762f856156d4322775b02f528
Abhilash Raj <raj.abhilash1@gmail.com>
2023-06-19 08:56:45 +0530

    fix: Use -P pager=off in the psql commands.
    This allows for non-interactive use and doesn't hang on the user
    input in certain conditions, like when the command output is large.
    
    Fixes #610

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -7,7 +7,7 @@ function wait_for_postgres () {
 	# moving forward.
 	# TODO: Use python's psycopg2 module to do this in python instead of
 	# installing postgres-client in the image.
-	until psql $DATABASE_URL -c '\l'; do
+	until psql-P pager=off $DATABASE_URL -c '\l'; do
 		>&2 echo "Postgres is unavailable - sleeping"
 		sleep 1
 	done
