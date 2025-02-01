2cd01ff122e8d9943b806a3c59be7ac2b813b870
Lance A. Brown <lance@bearcircle.net>
2025-02-01 05:36:49 +0000

    Align mailman-web/urls.py with upstream
    Align urls.py with upstream per issue #713

--- web/mailman-web/urls.py
+++ web/mailman-web/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 1998-2016 by the Free Software Foundation, Inc.
+# Copyright (C) 2023 by the Free Software Foundation, Inc.
 #
-# This file is part of Postorius.
+# This file is part of mailman-web.
 #
 # Postorius is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -16,19 +16,25 @@
 # You should have received a copy of the GNU General Public License along with
 # Postorius.  If not, see <http://www.gnu.org/licenses/>.
 
+
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path(r'', RedirectView.as_view(
-        url=reverse_lazy('list_index'),
-        permanent=True)),
-    path(r'postorius/', include('postorius.urls')),
-    path(r'hyperkitty/', include('hyperkitty.urls')),
-    path(r'', include('django_mailman3.urls')),
-    path(r'accounts/', include('allauth.urls')),
-    # Django admin
-    path(r'admin/', admin.site.urls),
+    path(
+        "",
+        RedirectView.as_view(url=reverse_lazy("list_index"), permanent=True),
+    ),
+    # Include alternate Postorius and HyperKitty URLs.
+    path("postorius/", include("postorius.urls")),
+    path("hyperkitty/", include("hyperkitty.urls")),
+    # Order counts for various links. Put the above first and the following
+    # after so the suggested Apache config still works.
+    path("mailman3/", include("postorius.urls")),
+    path("archives/", include("hyperkitty.urls")),
+    path("", include("django_mailman3.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("admin/", admin.site.urls),
 ]
